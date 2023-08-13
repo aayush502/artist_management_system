beedf887aa2df96eae9cce94628da4bdd568f2a2
Aayush502 <aayushhumagain23@gmail.com>
2023-08-13 16:32:16 +0545

    feat: add django registration form for the user

---
+++ users/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+class RegistrationForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'phone', 'dob', 'address', 'gender', 'password1', 'password2',]
+        widgets = {
+            'dob': DateInput(),
+            'phone': forms.fields.TextInput(attrs={'placeholder': '+977 9999999999'}),
+        }
+
